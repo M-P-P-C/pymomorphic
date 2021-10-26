373ddc1335283af2557ad287a74ad1c77413e038
M-P-P-C <marianopablo97@hotmail.com>
2021-10-26 13:31:28 +0200

    Setup tests for pytest

--- pymomorphic3_test/test_myfunctions.py
+++ pymomorphic3_test/test_myfunctions.py
@@ -1,9 +1,9 @@
 from pymomorphic3 import myfunctions as pmh
 from pymomorphic3 import myfunctions2 as pmh2
 
-import timeit
+import numpy as np
 
-import unittest
+import pytest
 
 '''
 python setup.py bdist_wheel
@@ -13,21 +13,22 @@ pip install dist/pymomorphic3-0.1.0-py3-none-any.whl --force-reinstall
 #def main():
 #    test_mod_hom()
 
-class TestKeyMethods(unittest.TestCase):
+class TestModulusMethod:
 
     def test_modulus_hom(self):
         
-        #assert pmh.mod_hom(1000,10000)==1000
+        assert pmh.mod_hom(1000,10000)==np.array([1000])
 
-        self.assertTrue(a==True)
+    def test_modulus_hom_neg(self):
+        
+        assert pmh.mod_hom(1000,10000)==np.array([1000])
+        #hom_key = pmh2.KEY(p = 10**13, L = 10**3, r=10**1, N = 50)
+        #pmh2.KEY()
 
-        hom_key = pmh2.KEY(p = 10**13, L = 10**3, r=10**1, N = 50)
-        pmh2.KEY()
-
-        hom_key.process_test()
+        #hom_key.process_test()
 
 
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
