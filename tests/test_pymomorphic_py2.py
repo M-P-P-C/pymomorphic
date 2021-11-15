e1624d837bb8edebd7508eda774dff1f94b251c7
M-P-P-C <marianopablo97@hotmail.com>
2021-11-15 16:03:46 +0100

    Add new tests

--- tests/test_pymomorphic_py2.py
+++ tests/test_pymomorphic_py2.py
@@ -19,7 +19,7 @@ pip install dist/pymomorphic3-0.1.0-py3-none-any.whl --force-reinstall
 #def main():
 #    test_mod_hom()
 
-class TestModulusMethod(unittest.TestCase):
+class TestModulus(unittest.TestCase):
 
     def test_modulus_hom(self):
         
@@ -29,7 +29,39 @@ class TestModulusMethod(unittest.TestCase):
         
         self.assertEqual(pmh.modulus([1000],[10000]),np.array([1000]))
 
+class TestEncMethod(unittest.TestCase):
 
+    def test_encryption_int(self):
+        
+        #Initialize some variables for testing purposes
+        my_p = 10**12
+        my_L = 10**3
+        my_r = 10**1
+        my_N = 5
+
+        m = [20]
+        m2 = [600]
+
+        #initialize KEY and HOM_OP
+        my_key = pmh.KEY(p = my_p , L = my_L, r = my_r , N = my_N)
+    
+        self.assertEqual(pmh.modulus([1000],[10000]),np.array([1000]))
+
+
+class TestEncMultiplicationMethod(unittest.TestCase):
+
+    def test_encrypted_multiplication(self):
+        #Initialize some variables for testing purposes
+        my_p = 10**12
+        my_L = 10**3
+        my_r = 10**1
+        my_N = 5
+
+        m = [20]
+        m2 = [600]
+
+        #initialize KEY and HOM_OP
+        my_operator = pmh.HOM_OP(p = my_p, L = my_L, r = my_r , N = my_N)
 '''
 class TestKeyClass(unittest.TestCase):
 
